2dcddf6f2f1943183655212aa7e5278a52ceab40
Filipe Liska <filipe.liska@gmail.com>
2022-05-13 18:08:54 -0300

    Exercicios do dia 02 JS

---
+++ Fundaments/4-Introduction-Javascript-Programming-Logic/dia-2-array-loop-for/exercicio-01.js
@@ -0,0 +1,31 @@
+let cars = ['Volvo', 'BMW', 'Audi']
+
+for (let = index = 0; index < cars.length; index += 1){
+  console.log(cars[index]);
+}
+
+//
+//
+
+let groceryList = ['Arroz', 'Feijão', 'Alface', 'Melancia']
+
+for (let = index = 0; index < groceryList.length; index += 1){
+  console.log(groceryList[index]);
+}
+
+//
+//
+
+let word = 'Filipe Liska'
+for (let letter of word){
+  console.log(letter);
+}
+
+//
+//
+
+let names = ['João', 'Maria', 'Antônio', 'Margarida']
+
+for (let words of names){
+  console.log(words);
+}
