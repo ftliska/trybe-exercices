60f57637ab2d8a362be983934a24be35966e3f83
Filipe Liska <filipe.liska@gmail.com>
2022-05-16 13:50:50 -0300

    Exercicios mentoria dia 3 bloco 4

--- Fundaments/4-Introduction-Javascript-Programming-Logic/dia-3-logica-de-programacao-algoritmos/exercicio-mentoria.js
+++ Fundaments/4-Introduction-Javascript-Programming-Logic/dia-3-logica-de-programacao-algoritmos/exercicio-mentoria.js
@@ -6,7 +6,7 @@
 // "Elemento não encontrado no array" 
 
 let array = [2, 5, 10, 20, 35, 40]
-let elemento = 50
+let elemento = 40
 let numero = 0
 
   for(let index = 0; index < array.length; index += 1){
@@ -37,5 +37,8 @@ for(let index = 1; index < 50; index += 1){
 console.log(string);
 
 // 3. Números divisíveis por 3 
+// 
 // Escreva um algoritmo que cria uma sequência numérica de inteiros de razão 1, o algoritmo deve receber antes de criar o valor inicial e final. 
-// Conte os números divisíveis por 3 da sequência e imprima no terminal condicionando: Se houver 50 ou mais: --------------------------------------- "Há 50 ou mais números diviseis por 3" --------------------------------------- Caso o contrário: -------------------------- "Sequência muito pequena." -------------------------- 
+// Conte os números divisíveis por 3 da sequência e imprima no terminal condicionando: 
+// Se houver 50 ou mais: "Há 50 ou mais números divisiveis por 3" 
+// Caso o contrário: "Sequência muito pequena."
