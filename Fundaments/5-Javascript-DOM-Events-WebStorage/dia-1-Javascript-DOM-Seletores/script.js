46b1d15756104d9e5cfe26fc3a2a9e5ee063b2ff
Filipe Liska <filipe.liska@gmail.com>
2022-05-20 13:31:02 -0300

    Exercício 01

---
+++ Fundaments/5-Javascript-DOM-Events-WebStorage/dia-1-Javascript-DOM-Seletores/script.js
@@ -0,0 +1,18 @@
+/*
+        Aqui você vai modificar os elementos já existentes utilizando apenas as funções:
+        - document.getElementById()
+        - document.getElementsByClassName()
+        - document.getElementsByTagName()
+ Crie uma função que corrija o texto da tag <h1>.
+ Crie uma função que modifique todo o texto da tag <p> para maiúsculo.
+ Crie uma função que exiba o conteúdo de todas as tags <p> no console.
+        */
+
+//  Crie uma função que mude o texto na tag <p> para uma descrição de como você se vê daqui a 2 anos. (Não gaste tempo pensando no texto e sim realizando o exercício)
+
+function changeParagraph() {
+  let changeParagraph = document.getElementsByTagName('p')[1].innerText = '2 anos mais velho';
+
+  return changeParagraph;
+}
+changeParagraph();
