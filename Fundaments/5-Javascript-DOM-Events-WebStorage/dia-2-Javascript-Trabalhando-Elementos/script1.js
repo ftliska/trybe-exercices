108482e7a4322645afad69ad3a9cfd5f179da834
Filipe Liska <filipe.liska@gmail.com>
2022-05-23 13:12:08 -0300

    Exercicio 2

--- Fundaments/5-Javascript-DOM-Events-WebStorage/dia-2-Javascript-Trabalhando-Elementos/script1.js
+++ Fundaments/5-Javascript-DOM-Events-WebStorage/dia-2-Javascript-Trabalhando-Elementos/script1.js
@@ -3,3 +3,8 @@ let title = document.createElement('h1')
 title.innerText = 'Exercício 5.2 - JavaScript DOM'
 document.body.appendChild(title)
 
+//Adicione a tag main com a classe main-content como filho da tag body;
+let m = document.createElement('main')
+m.className = 'main-content'
+document.body.appendChild(m)
+
