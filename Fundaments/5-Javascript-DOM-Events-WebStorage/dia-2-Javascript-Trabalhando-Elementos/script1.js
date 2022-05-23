55b6e7f9cc6b30d9cb374c8fc0cea54a2c5ccdff
Filipe Liska <filipe.liska@gmail.com>
2022-05-23 13:13:25 -0300

    Exercicio 5

--- Fundaments/5-Javascript-DOM-Events-WebStorage/dia-2-Javascript-Trabalhando-Elementos/script1.js
+++ Fundaments/5-Javascript-DOM-Events-WebStorage/dia-2-Javascript-Trabalhando-Elementos/script1.js
@@ -14,3 +14,15 @@ let sec = document.createElement('section')
 sec.className = 'center-content'
 m.appendChild(sec);
 
+//Adicione a tag p como filho do section criado no passo 3 e coloque algum texto;
+
+let paragraph = document.createElement('p');
+paragraph.innerText = 'Algum texto';
+sec.appendChild(paragraph);
+
+//Adicione a tag section com a classe left-content como filho da tag main criada no passo 2;
+
+let secLeft = document.createElement('section');
+secLeft.className = 'left-content';
+m.appendChild(secLeft);
+
