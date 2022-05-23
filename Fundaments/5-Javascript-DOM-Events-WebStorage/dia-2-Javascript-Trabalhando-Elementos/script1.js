a555d78357be38e1d0af447770315a6b947c5617
Filipe Liska <filipe.liska@gmail.com>
2022-05-23 13:12:23 -0300

    Exercicio 4

--- Fundaments/5-Javascript-DOM-Events-WebStorage/dia-2-Javascript-Trabalhando-Elementos/script1.js
+++ Fundaments/5-Javascript-DOM-Events-WebStorage/dia-2-Javascript-Trabalhando-Elementos/script1.js
@@ -8,3 +8,9 @@ let m = document.createElement('main')
 m.className = 'main-content'
 document.body.appendChild(m)
 
+//Adicione a tag section com a classe center-content como filho da tag main criada no passo 2;
+
+let sec = document.createElement('section')
+sec.className = 'center-content'
+m.appendChild(sec);
+
