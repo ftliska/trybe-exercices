83b9b7c76b6c15ec965ba4a2a1142c6fa62ee517
Filipe Liska <filipe.liska@gmail.com>
2022-05-24 15:27:34 -0300

    Exercicio 1 dia 3 bloco 5

--- Fundaments/5-Javascript-DOM-Events-WebStorage/dia-3-Javascript-Eventos/script.js
+++ Fundaments/5-Javascript-DOM-Events-WebStorage/dia-3-Javascript-Eventos/script.js
@@ -14,3 +14,33 @@ function createDaysOfTheWeek() {
 createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
+
+const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+// Função que cria dinamicamente cada dia do calendário e os adiciona como filhos/filhas da tag <ul> com ID "days".
+//Os dias devem estar contidos em uma tag <li>, e todos devem ter a classe day
+//Os dias 24, 25 e 31 são feriados e, além da classe day, devem conter também a classe holiday.
+//Os dias 4, 11, 18 e 25 são Sexta-feira. Eles devem conter a classe day e a classe friday.
+
+function createDates() {
+  let daysList = document.querySelector('#days');
+  for (let index = 0; index < dezDaysList.length; index += 1) {
+    let day = dezDaysList[index];
+    let li = document.createElement('li');
+    daysList.appendChild(li)
+    li.className = 'day';
+    li.innerText = day;
+    if (day === 24 | day === 31) {
+      li.className = 'day holiday';
+    } else if (day === 4 | day === 11 | day === 18) {
+      li.className = 'day friday';
+    } else if (day === 25) {
+      li.className = 'day holiday friday';
+    }
+  }
+}
+
+
+
+
+
+createDates()
