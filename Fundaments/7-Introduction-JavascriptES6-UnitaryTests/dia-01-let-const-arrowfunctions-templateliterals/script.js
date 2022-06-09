72f7cf5bb6e50278bf6d334802f31540bcc66965
Filipe Liska <filipe.liska@gmail.com>
2022-06-09 11:44:03 -0300

    Exercicio 3 Dia 1 Bloco 7

--- Fundaments/7-Introduction-JavascriptES6-UnitaryTests/dia-01-let-const-arrowfunctions-templateliterals/script.js
+++ Fundaments/7-Introduction-JavascriptES6-UnitaryTests/dia-01-let-const-arrowfunctions-templateliterals/script.js
@@ -1,72 +1,88 @@
 // Parte 1
 // Exercício 1
 
-const testingScope = (escopo) => {
+const testingScope = escopo => {
   if (escopo === true) {
-    let ifScope = `Não devo ser utilizada fora do meu escopo (if)`;
-    ifScope = `${ifScope} ótimo, fui utilizada no escopo!`;
-    console.log(ifScope);
+    let ifScope = `Não devo ser utilizada fora do meu escopo (if)`
+    ifScope = `${ifScope} ótimo, fui utilizada no escopo!`
+    console.log(ifScope)
   } else {
-    const elseScope = `Não devo ser utilizada fora meu escopo (else)`;
-    console.log(elseScope);
+    const elseScope = `Não devo ser utilizada fora meu escopo (else)`
+    console.log(elseScope)
   }
 }
-testingScope(true);
+testingScope(true)
 
 // Exercício 2
 
-const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+const oddsAndEvens = [13, 3, 4, 10, 7, 2]
 
 const sortOddsAndEvens = () => {
-  oddsAndEvens[0] = 2;
-  oddsAndEvens[1] = 3;
-  oddsAndEvens[2] = 4;
-  oddsAndEvens[3] = 7;
-  oddsAndEvens[4] = 10;
-  oddsAndEvens[5] = 13;
-  
+  oddsAndEvens[0] = 2
+  oddsAndEvens[1] = 3
+  oddsAndEvens[2] = 4
+  oddsAndEvens[3] = 7
+  oddsAndEvens[4] = 10
+  oddsAndEvens[5] = 13
+
   return oddsAndEvens
 }
-const sortedArray = sortOddsAndEvens();
-console.log(sortedArray);
+const sortedArray = sortOddsAndEvens()
+console.log(sortedArray)
 
-const compararNumeros = (a, b) => a - b;
+const compararNumeros = (a, b) => a - b
 
-oddsAndEvens.sort(compararNumeros);
-console.log(oddsAndEvens);
+oddsAndEvens.sort(compararNumeros)
+console.log(oddsAndEvens)
 
 // Parte 2
 // Exercício 1
 
-const factorial1 = (n) => {
-  let result = 1;
+const factorial1 = n => {
+  let result = 1
 
   for (let i = 2; i <= n; i += 1) {
-    result *= i;
+    result *= i
   }
-  return result;
+  return result
 }
 
-console.log(factorial1(5));
+console.log(factorial1(5))
 
-const factorial = n => n > 1 ? n * factorial(n - 1) : 1;
-console.log(factorial(4));
+const factorial = n => (n > 1 ? n * factorial(n - 1) : 1)
+console.log(factorial(4))
 
-// Parte 3
+// Parte 2
 // Exercício 2
 
-const getLongestWord = (phrase) => {
-  let wordsArray = phrase.split(' ');
-  let maxLength = 0;
-  let result = '';
+const getLongestWord = phrase => {
+  let wordsArray = phrase.split(' ')
+  let maxLength = 0
+  let result = ''
 
   for (let biggest of wordsArray) {
     if (biggest.length > maxLength) {
-      maxLength = biggest.length;
-      result = biggest;
+      maxLength = biggest.length
+      result = biggest
     }
   }
-  return result;
+  return result
 }
-console.log(getLongestWord('eu queria ser uma abelha pra pousar na sua flor'));
+console.log(
+  getLongestWord(
+    'Posso não concordar com o que você diz, mas defenderei até a morte o seu direito de dizê-lo'
+  )
+)
 
+// Parte 3
+// Exercício 3
+
+const btn = document.querySelector('#btn')
+const counter = document.querySelector('#counter')
+counter.innerText = 0;
+let count = 0;
+
+btn.addEventListener('click', () => {
+  count += 1;
+  counter.innerText = count;
+})
