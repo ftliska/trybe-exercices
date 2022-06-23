391854650068075f4e002bed2decdf2554e4d8a7
Filipe Liska <filipe.liska@gmail.com>
2022-06-23 19:37:51 -0300

    Para fixar 5 dia 2 Bloco 8

--- Fundaments/8-Higher-Order-Functions-JavascriptES6/dia-2-forEach-find-some-every-sort/parafixar.js
+++ Fundaments/8-Higher-Order-Functions-JavascriptES6/dia-2-forEach-find-some-every-sort/parafixar.js
@@ -4,46 +4,54 @@ const emailListInData = [
   'roberta@email.com',
   'paulo@email.com',
   'anaroberta@email.com',
-  'fabiano@email.com',
-];
+  'fabiano@email.com'
+]
 
-const showEmailList = (email) => {
-  console.log(`O email ${email} esta cadastrado em nosso banco de dados!`);
-};
+const showEmailList = email => {
+  console.log(`O email ${email} esta cadastrado em nosso banco de dados!`)
+}
 
-const xablau = emailListInData.forEach(showEmailList);
-console.log(xablau); 
+const xablau = emailListInData.forEach(showEmailList)
+console.log(xablau)
 
 // 2 - Utilize o find para retornar o primeiro número do array que é divisível por 3 e 5, caso ele exista:
 
-const numbers = [19, 21, 30, 3, 45, 22, 15];
+const numbers = [19, 21, 30, 3, 45, 22, 15]
 
 const findDivisibleBy3And5 = () => {
-  return numbers.find((number) => number % 3 === 0 && number % 5 === 0);
+  return numbers.find(number => number % 3 === 0 && number % 5 === 0)
 }
 
-console.log(findDivisibleBy3And5()); 
+console.log(findDivisibleBy3And5())
 
 // 3 - Utilize o find para encontrar o primeiro nome com cinco letras, caso ele exista:
 
-const names = ['João', 'Iranilda', 'Fernando', 'Maria'];
+const names = ['João', 'Iranilda', 'Fernando', 'Maria']
 
-const findNameWithFiveLetters = () => names.find((name) => name.length === 5);
+const findNameWithFiveLetters = () => names.find(name => name.length === 5)
 
-console.log(findNameWithFiveLetters()); 
+console.log(findNameWithFiveLetters())
 
 // 4 - Utilize o find para encontrar a música com id igual a 31031685, caso ela exista:
 
 const musicas = [
   { id: '31031685', title: 'Partita in C moll BWV 997' },
   { id: '31031686', title: 'Toccata and Fugue, BWV 565' },
-  { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' },
+  { id: '31031687', title: 'Chaconne, Partita No. 2 BWV 1004' }
 ]
 
 function findMusic(id) {
-  return musicas.find((musica) => musica.id === id).title;
+  return musicas.find(musica => musica.id === id).title
 }
 
-console.log(findMusic('31031686'));
+console.log(findMusic('31031686'))
+
+// 5 - Escreva uma função que dado um array de nomes e um nome retorne true se ele estiver contido e caso contrário, retorne false, use some;
+
+const names1 = ['Mateus', 'José', 'Ana', 'Cláudia', 'Bruna']
+
+const hasName = (arr, name) => arr.some(currname => currname === name);
+
+console.log(hasName(names1, 'Ana'))
 
 
